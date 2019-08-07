f99ac435c30e80a81c9a5ce1c002959435772d11
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-08-07 19:16:48 -0400

    Save exercises test

--- test/exercises.js
+++ test/exercises.js
@@ -3,19 +3,25 @@
 const chai      = require('chai');
 const chaiHTTP  = require('chai-http');
 const app       = require('../app');
+const Exercise  = require('../models/exercise');
 
 const should  = chai.should;
 chai.use(chaiHTTP);
 
 describe('api/exercises', () => {
+  
+  beforeEach( (done) => {
+    Exercise.deleteMany({}, (err, res) => done());
+  });
 
-  describe("GET /test", () => {
+  describe("GET /test", (done) => {
     it("should return '/api/exercises/ works!'", () => {
       chai.request(app)
         .get('/api/exercises/test')
         .end((error, res) => {
           res.should.have.status(200);
-        })
+          done();
+        });
     });
-  })
+  });
 });

7ab776f0a9cedff7a2e4e82a6180b1d6885e9223
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-08-22 22:05:10 -0400

    Fix bug: remove localStorage before account delete

--- client/src/App/services/Account/actions.js
+++ client/src/App/services/Account/actions.js
@@ -2,6 +2,7 @@ import { SET_CURRENT_USER, GET_ERRORS } from '../types';
 import axios from 'axios';
 
 export const deleteAccount = () => dispatch => {
+  localStorage.removeItem('exetrakerToken');
   axios
     .delete('/api/users/account')
     .then(res => dispatch({
